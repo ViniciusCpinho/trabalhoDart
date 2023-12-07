343383a8bfa3807efb54f9f5439d3705dcef3c94
Vinicius Pinheiro <carraravinicius155@gmail.com>
2023-12-06 20:08:25 -0400

    Adicionar arquivos para GitHub Pages

--- android/app/src/main/kotlin/com/example/cozinha_fora_da_caixa/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.cozinha_fora_da_caixa
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/api/api.dart
+++
@@ -1,8 +0,0 @@
-import 'package:http/http.dart' as http;
-
-
-class ApiConfig {
-  static const String baseUrl = 'https://656b81a3dac3630cf7282c58.mockapi.io/';
-}
-
-
